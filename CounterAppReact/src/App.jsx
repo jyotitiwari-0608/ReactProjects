d3c3abf157fbbccd1fc9ac871a69d84eba4297c6
Jyoti Tiwari <jt933558@gmail.com>
2026-02-13 19:40:20 +0530

    UI updation using hooks in reactState in react

---
+++ CounterAppReact/src/App.jsx
@@ -0,0 +1,23 @@
+// UI updation , react state , hooks ke through ho data update hoga , ui updation ki power react ke paas h
+// useState is a hook , hook call : 
+import {useState} from 'react'
+function App() {
+  let [count,setCounter]=useState(0);
+  // let count = 0;
+  // let a;
+  let increment = ()=>{  setCounter(count+1);};
+  let decrement = ()=>{  setCounter(count-1);};
+
+
+  return(
+    <>
+  <h1>Chai aur React</h1>
+  <h2>Counter Value :{count}</h2>
+  <button onClick={increment}>Add Value</button>
+  <br/>
+  <button onClick={decrement}>Remove Value</button>
+  </>
+  )
+}
+
+export default App
